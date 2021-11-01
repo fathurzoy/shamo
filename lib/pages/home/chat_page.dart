0d85846babee5f6a4bc0800b7e524726f0efd2ac
fathurzoy <fathurrahmanf123@gmail.com>
2021-11-01 11:23:50 +0700

    Chat Page Bagian Content

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -22,7 +23,7 @@ class ChatPage extends StatelessWidget {
       );
     }
 
-    Widget content() {
+    Widget emptyChat() {
       return Expanded(
         child: Container(
           width: double.infinity,
@@ -78,6 +79,19 @@ class ChatPage extends StatelessWidget {
                 ),
               ),
             ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+            children: [ChatTile()],
           ),
         ),
       );
