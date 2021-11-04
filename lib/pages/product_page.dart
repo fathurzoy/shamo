e3e354a5e741e0f4ffa88bccf48b6aa09c71a115
fathurzoy <fathurrahmanf123@gmail.com>
2021-11-05 06:51:24 +0700

    Product Page Bagian Product Slider

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,12 +1,40 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png'
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        margin: EdgeInsets.symmetric(
+          horizontal: 2,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: currentIndex == index ? primaryColor : Color(0xffc4c4c4),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
+
       return Column(
         children: [
           Container(
@@ -33,6 +61,36 @@ class ProductPage extends StatelessWidget {
               ],
             ),
           ),
+          CarouselSlider(
+            items: images
+                .map(
+                  (image) => Image.asset(
+                    image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 310,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              index++;
+              return indicator(index);
+            }).toList(),
+          )
         ],
       );
     }
